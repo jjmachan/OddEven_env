26b80c38ab93b257225ff773aa71576a0a3b4deb
jjmachan <jamesjithin97@gmail.com>
2018-09-13 01:36:52 +0530

    finished step() reset()

--- OddEven_env/envs/oddoreven_env.py
+++ OddEven_env/envs/oddoreven_env.py
@@ -1,28 +1,45 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import random
+import os
 
 class OddorevenEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
-  def __init__(self,small=2,large=10):
-    self.input=0
+  def __init__(self,max_reward=100):
+    self.max_reward = max_reward
     self.state=0
+    self.counter=0
     self.action_space = spaces.Discrete(5)
     self.observation_space = spaces.Discrete(5)
     self.seed()
+    with open('player-profile-1.txt') as f:
+      self.lines = f.read().splitlines()
+    #print(self.lines)
 
-  def _seed(self, seed=None):
+  def seed(self, seed=None):
     self.np_random, seed = seeding.np_random(seed)
     return [seed]    
    
   
-  def _step(self, action):
-    return 0, 0, 0, {} 
+  def step(self, action):
+    done = False
+    assert self.action_space.contains(action)
+    self.counter += 1
+    self.state = int(self.lines[self.counter])
+    if action == self.state:
+      done = True
+      reward = self.max_reward - (self.counter/(self.counter+5))*self.max_reward  # formula for 
+    else:
+      reward = 0
+    return self.state, reward, done, {} 
 
-  def _reset(self):
-    return 0     
+  def reset(self):
+    self.state = random.randrange(1,6)
+    self.counter = 0
+    return self.state     
     
-  def _render(self, mode='human', close=False):
+  def render(self, mode='human', close=False):
     return 0     
     
